e50445df21824f09eb7e0dd2e712dd6b7527cce8
Pasindu Ruwandeniya <pasindujayanada@gmail.com>
2021-05-18 14:03:51 +0530

    Completed "Displaying cards" section. "playlist and play" is WIP. Updated readme.md

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
-# Soundcloud Player
-Simple Soundcloud playlist creator using Soundcloud API.
+# SoundCloud Player
+Simple SoundCloud playlist creator using Soundcloud API.
 
-1. I develop these simple Soundcloud player to sharpen my JavaScript knowledge.
+1. I develop these simple SoundCloud player to sharpen my JavaScript knowledge.
 2. In `main.js` file there are 4 main sections to resolve as you can see.
 3. Those 4 sections should be developed to get the final product "Soundcloud Player".
 4. In the future there could be subsections as I develop.
-5. As the time of writing this blog post Soundcloud has stopped providing free API keys, so I'm gonna use a public API key that I found on the Internet. *might get broken, let's see*
+5. As the time of writing this blog post SoundCloud has stopped providing free API keys, so I'm gonna use a public API key that I found on the Internet. *might get broken, let's see*
 5. This post is going to get updated as I develop this.
 
-## Developing "Query Soundcloud API" section.
+## Query Soundcloud API.
 
 1. Even though this is the 2nd section, I want to tackle this first.
 2. You can read more about the "Search" function in [Soundcloud docs.](https://developers.soundcloud.com/docs/api/guide#search) 
-3. I copied the code in Soundcloud docs and refactored as I want (Removed `<script>` tags, removed license filter and put the "Client Id")
+3. I copied the code in SoundCloud docs and refactored as I want (Removed `<script>` tags, removed license filter and put the "Client Id")
 ```
 SC.initialize({
   client_id: 'Client Id'
@@ -46,4 +46,22 @@ SC.get('/tracks', {
 
 SoundCloudAPI.getTracks("Sinhala songs");
 ```
-5. Created `renderTracks()` function to print/render all the result tracks as cards, so the cards can be rendered dynamically and easily looped through.
+
+## Displaying Cards.
+
+1. Created `renderTracks()` function to print/render all the result tracks as cards, so the cards can be rendered dynamically and easily looped through.
+2. Inside the function I created the card dynamically using `createElement()`, `classList` and `appendChild()` methods.
+3. Finally called the API endpoints dynamically.
+
+## Add to playlist and play.
+
+1. Grabbed the code for embedded player from [SoundCloud docs.](https://developers.soundcloud.com/docs/api/sdks#embedding)
+```
+SC.oEmbed('https://soundcloud.com/forss/flickermood', {
+  auto_play: true
+}).then(function(embed){
+  console.log('oEmbed response: ', embed);
+});
+```
+4. Modified the code to place the "embedded player" on the left sidebar and pass the music dynamically (Music passing `clickEvent` is coded inside "Displaying Cards" section)
+5. Used `insertBefore()` method to stack the music in the `.js-playlist` div.
--- javascript/main.js
+++ javascript/main.js
@@ -17,49 +17,87 @@ SoundCloudAPI.getTracks = function (inputValue) {
 		q: inputValue,
 	}).then(function (tracks) {
 		console.log(tracks);
+		SoundCloudAPI.renderTracks(tracks);
 	});
 };
 
 SoundCloudAPI.getTracks("Sinhala songs");
 
-SoundCloudAPI.renderTracks = function () {
-	var card = document.createElement("div");
-	card.classList.add("card");
-	var searchResults = document.querySelector(".js-search-results");
-	searchResults.appendChild(card);
-
-	var image = document.createElement("div");
-	image.classList.add("image");
-	var imageAndContentSelector = document.querySelector(".card");
-	imageAndContentSelector.appendChild(image);
-
-  var img = document.createElement("img");
-  img.classList.add("image_img");
-  var imgSelector = document.querySelector(".image");
-
-  var content = document.createElement("div");
-  content.classList.add("content");
-  imageAndContentSelector.appendChild(content);
-
-  var ui = document.createElement("div");
-  ui.classList.add = ("ui", "bottom", "attached", "button", "js-button");
-  var uiSelector = document.querySelector(".card");
-  uiSelector.appendChild(ui);
-
-  var icon = document.createElement("i");
-  icon.classList.add = ("add", "icon");
-  var iconAndSpanSelector = document.querySelector(".ui");
-  iconAndSpanSelector.appendChild(icon);
-
-  var span = document.createElement("span");
-  iconAndSpanSelector.appendChild(span);
-  span.innerHTML = ("Add to playlist");
-
-
-};
-
-SoundCloudAPI.renderTracks();
-
 // 3. Display the cards.
 
+SoundCloudAPI.renderTracks = function (tracks) {
+	tracks.forEach(function (track) {
+		//card
+		var card = document.createElement("div");
+		card.classList.add("card");
+
+		//image
+		var imageDiv = document.createElement("div");
+		imageDiv.classList.add("image");
+
+		var image_img = document.createElement("img");
+		image_img.classList.add("image_img");
+		image_img.src =
+			track.artwork_url ||
+			"https://cdn.icon-icons.com/icons2/2592/PNG/512/soundcloud_logo_icon_154496.png";
+
+		imageDiv.appendChild(image_img);
+
+		//content
+		var content = document.createElement("div");
+		content.classList.add("content");
+
+		var header = document.createElement("div");
+		header.classList.add("header");
+		header.innerHTML =
+			"<a href = " +
+			track.permalink_url +
+			" target = _blank>" +
+			track.title +
+			"</a>";
+
+		//button
+		var button = document.createElement("div");
+		button.classList.add("ui", "bottom", "attached", "button", "js-button");
+
+		var icon = document.createElement("i");
+		icon.classList.add("add", "icon");
+
+		var buttonText = document.createElement("span");
+		button.innerHTML = "Add to playlist ";
+
+		//appendChild
+		content.appendChild(header);
+
+		button.appendChild(icon);
+		button.appendChild(buttonText);
+
+		button.addEventListener("click", function () {
+			SoundCloudAPI.getEmbed(track.permalink_url);
+		});
+
+		card.appendChild(imageDiv);
+		card.appendChild(content);
+		card.appendChild(button);
+
+		var searchResults = document.querySelector(".js-search-results");
+		searchResults.appendChild(card);
+	});
+};
+
 // 4. Add to playlist and play.
+
+SoundCloudAPI.getEmbed = function (trackURL) {
+	SC.oEmbed(trackURL, {
+		auto_play: false,
+	}).then(function (embed) {
+		console.log("oEmbed response: ", embed);
+
+		var sideBar = document.querySelector(".js-playlist");
+
+		var box = document.createElement("div");
+		box.innerHTML = embed.html;
+
+		sideBar.insertBefore(box, sideBar.firstChild);
+	});
+};
